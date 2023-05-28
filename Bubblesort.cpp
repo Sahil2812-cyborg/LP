d08e2c3dff160c8a6a95f38672a1053b1f7ad5c3
Sahil Ratnaparkhi <67883288+Sahil2812-cyborg@users.noreply.github.com>
2023-05-28 19:14:59 +0530

    Bubble sort using openmp

---
+++ Bubblesort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <omp.h>
+
+void bubbleSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        #pragma omp parallel for
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    bubbleSort(arr, size);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+    
+    return 0;
+}
+
